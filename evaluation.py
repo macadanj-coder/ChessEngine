5fd493d27bd289dd7589407363d11f6a5d4b3baa
macadanj-coder <joshryanmac@hotmail.com>
2025-11-18 21:21:09 -0500

    Added eval_board, eval_piece, and pawn_table

--- evaluation.py
+++ evaluation.py
@@ -9,18 +9,36 @@ piece_values = {
     chess.KING: 20000
 }
 
-board = chess.Board(chess.STARTING_FEN)
-white_material = 0
-black_material = 0
+pawn_table_white = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5, -5,-10,  0,  0,-10, -5,  5,
+    5, 10, 10,-20,-20, 10, 10,  5,
+    0,  0,  0,  0,  0,  0,  0,  0
+]
+pawn_table_black = list(reversed(pawn_table_white))
 
-for square in chess.SQUARES:
-    piece = board.piece_at(square)
-    if not piece:
-        continue
-    if piece.color == chess.WHITE:
-        white_material += piece_values[piece.piece_type]
+def eval_piece(piece: chess.Piece, square: chess.Square) -> int:
+    mapping = []
+    if piece.piece_type == chess.PAWN:
+        mapping = pawn_table_white if piece.color == chess.WHITE else pawn_table_black
     else:
-        black_material += piece_values[piece.piece_type]
+        return 0
+    return mapping[square]
 
-print("White material value: ", white_material)
-print("Black material value:", black_material)
+
+def eval_board(board: chess.Board) -> float:
+    print(board)
+    total = 0
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if not piece:
+            continue
+        value = piece_values[piece.piece_type] + eval_piece(piece, square)
+        total += value if piece.color == chess.WHITE else -value
+    return total
+
+print(eval_board(chess.Board()))
